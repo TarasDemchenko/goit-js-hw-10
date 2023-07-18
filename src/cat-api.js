2856f5462d95a742a37adf405f5349b745441cdb
TarasDemchenko <tarasdemchenko1993@gmail.com>
2023-07-18 21:12:41 +0200

    home work 10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
 async function fetchBreeds() {
   try {
     const response = await axios.get('https://api.thecatapi.com/v1/breeds');
@@ -11,4 +16,50 @@ async function fetchBreeds() {
   }
 }
 
-export { fetchBreeds };
+async function fetchCatByBreed(breedId) {
+  try {
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    );
+
+    if (response.status === 200 && response.data.length > 0) {
+      const catData = response.data[0];
+      const breed = catData.breeds[0];
+
+      const image = document.createElement('img');
+      image.className = 'image';
+      image.src = catData.url;
+
+      const catName = document.createElement('h2');
+      catName.textContent = breed.name;
+
+      const catDescription = document.createElement('p');
+      catDescription.textContent = breed.description;
+
+      const catTemperament = document.createElement('p');
+      catTemperament.textContent = `Temperament: ${breed.temperament}`;
+
+      catInfo.innerHTML = '';
+      catInfo.appendChild(image);
+      catInfo.appendChild(catName);
+      catInfo.appendChild(catDescription);
+      catInfo.appendChild(catTemperament);
+
+      loader.style.display = 'none';
+      catInfo.style.display = 'block';
+
+      error.style.display = 'none';
+    } else {
+      handleError();
+    }
+  } catch (error) {
+    handleError();
+  }
+}
+
+function handleError() {
+  loader.style.display = 'none';
+  error.style.display = 'block';
+}
+
+export { fetchBreeds, fetchCatByBreed, breedSelect, loader, error, catInfo };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { fetchBreeds } from './cat-api.js';
-import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import axios from 'axios';
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
@@ -8,47 +8,6 @@ const catInfo = document.querySelector('.cat-info');
 
 axios.defaults.headers.common['x-api-key'] =
   'live_rjq7oc9H58wfDo8wJKSIgim6FnP6u7owyB6MlHstz0oICkzgbjXAiz65ab977DSO';
-
-async function fetchCatByBreed(breedId) {
-  try {
-    const response = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    );
-
-    if (response.status === 200 && response.data.length > 0) {
-      const catData = response.data[0];
-      const breed = catData.breeds[0];
-
-      const image = document.createElement('img');
-      image.className = 'image';
-      image.src = catData.url;
-
-      const catName = document.createElement('h2');
-      catName.textContent = breed.name;
-
-      const catDescription = document.createElement('p');
-      catDescription.textContent = breed.description;
-
-      const catTemperament = document.createElement('p');
-      catTemperament.textContent = `Temperament: ${breed.temperament}`;
-
-      catInfo.innerHTML = '';
-      catInfo.appendChild(image);
-      catInfo.appendChild(catName);
-      catInfo.appendChild(catDescription);
-      catInfo.appendChild(catTemperament);
-
-      loader.style.display = 'none';
-      catInfo.style.display = 'block';
-
-      error.style.display = 'none';
-    } else {
-      handleError();
-    }
-  } catch (error) {
-    handleError();
-  }
-}
 
 function handleError() {
   loader.style.display = 'none';
@@ -61,7 +20,7 @@ breedSelect.addEventListener('change', async event => {
   catInfo.style.display = 'none';
 
   try {
-    await fetchCatByBreed(breedId);
+    await fetchCatByBreed(breedId, catInfo, loader, error);
   } catch (error) {
     handleError();
   }
